a4a1edfd49c8fa558c765d5ce0f05f746fb09aab
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-13 01:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function logDriversByHometown(drivers, location) {
   const qualifiedDrivers = drivers.filter(driver => driver.hometown === location);
   logDriverNames(qualifiedDrivers);
 }
+
+function driversByRevenue(drivers) {
+  const numberSorter = function ( num1, num2) {
+    return num1.revenue - num2.revenue;
+  }
+  
+  drivers.sort(comparator)
+}
