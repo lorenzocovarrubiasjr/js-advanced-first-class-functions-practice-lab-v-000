c9d3c7e1ca01ae45485af29e320279cd168c70ae
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-13 00:51:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  drivers.forEach(console.log(driver.name))
+  drivers.forEach(function(driver) {
+    console.log(driver.name))
+  }
 }
